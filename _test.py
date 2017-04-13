23094ba02d7a2a19c0309d4a6d329d9e48a5d656
christopher <christopher.r.wahl@gmail.com>
2017-04-13 10:52:03 -0400

    ARCHIVE:
    Removed a number of testing files.
    
    Re-worked chi() to simplify all the testing code and operate in such a way that it does not modify any of the Spectrum objects it is passed.
    
    Documented analysis.chi

--- _test.py
+++ _test.py
@@ -1,1 +1,14 @@
+from catalog import get_source_ns, load_shen_source, shenCat
 
+data = load_shen_source()[ 1 ]
+
+i = 0
+for dp in data:
+    ns = get_source_ns( dp )
+    if ns in shenCat:
+        shenCat[ ns ][ 'lum_mgii' ] = dp[ 83 ]
+        shenCat[ ns ][ 'lum_mgii_err' ] = dp[ 84 ]
+        shenCat[ ns ][ 'fwhm_mgii' ] = dp[ 85 ]
+        shenCat[ ns ][ 'fwhm_mgii_err' ] = dp[ 86 ]
+        i += 1
+        print( f"{ns}: {i} : {shenCat[ ns ]}" )
--- analysis/chi.py
+++ analysis/chi.py
@@ -1,184 +1,68 @@
-from typing import Dict, Tuple
+from typing import Dict
 
-from spectrum import List, Spectrum, Union
+from spectrum import Iterable, Spectrum, Tuple
 
 
-def chi( expSpec: Spectrum, obsSpec: Spectrum, doScale: bool = False, skipCopy: bool = False,
-         wl_low_limit: float = None, wl_high_limit: float = None, n_sigma: float = 1,
-         old_process: bool = False, get_count: bool = False, skip_2cpy = False ) -> float:
+def __chi_value( prime_point: Tuple[ float, float ], sec_point: Tuple[ float, float ], n_sigma: float ) -> float:
+    err = (prime_point[ 1 ] + sec_point[ 1 ]) * n_sigma
+    diff = abs( prime_point[ 0 ] - sec_point[ 0 ] )
+    return pow( diff, 2 ) / prime_point[ 0 ] if err > diff else 0
+
+
+def chi( primary: Spectrum, secondary: Spectrum, wl_low: float = None, wl_high: float = None, n_sigma=1 ) -> float:
     """
-    Returns the chi^2 value between the given spectra
+    Simple chi^2 matching system.  This method does make any modifications to the Spectrum objects passed in.  Note that
+    if no wavelegnth limitations are given, the bounds of the objects will be used.
 
-    :param expSpec: Expected Spectrum
-    :param obsSpec: Observed Spectrum
-    :param doScale: Scale the two spectra around DEF_SCALE_WL range
-    :param wl_low_limit: Trim spectra to low limit before matching
-    :param wl_high_limit: Trim spectra to high limit before matching
-    :param n_sigma: Multiplicative flux-err range to zero.  ie. Any value falling within flux +/- n_sigma * err = 0
-    :type expSpec: Spectrum
-    :type obsSpec: Spectrum
-    :type doScale: bool
-    :type wl_low_limit: float
-    :type wl_high_limit: float
+    :param primary: Primary spectrum to be checked against 
+    :type primary: Spectrum
+    :param secondary: Secondary spectrum to be checked against
+    :type secondary: Spectrum
+    :param wl_low: Minimum wavelength to begin checking at.  Defaults to None
+    :type wl_low: float
+    :param wl_high: Maximum wavelegnth at which to end checking.  Defaults to None
+    :type wl_high: float
+    :param n_sigma: Error bound multiplier in which to define the overlap range where chi value is zero.  Defaults to 1.
     :type n_sigma: float
-    :return: chi^2
+    :return: Chi^2 value over the two spectra
     :rtype: float
     """
-    if skip_2cpy:
-        a0 = expSpec.cpy( )
-        a1 = obsSpec
-    elif not skipCopy:
-        a0 = expSpec.cpy( )
-        a1 = obsSpec.cpy( )
-    else:
-        a0 = expSpec
-        a1 = obsSpec
-
-    if( doScale ):
-        a1.scale( spec = a0 )
-
-    if wl_low_limit is not None:
-        a0.trim( wlLow=wl_low_limit )
-        a1.trim( wlLow=wl_low_limit )
-
-    if wl_high_limit is not None:
-        a0.trim( wlHigh=wl_high_limit )
-        a1.trim( wlHigh=wl_high_limit )
-
-    a0.alignToSpec( a1 )
-
-    # old system, uncontrolled for error.  Left in for testing.
-    if old_process:
-        s = sum( [ pow( a0.getFlux( wl ) - a1.getFlux( wl ), 2 ) / a0.getFlux( wl ) for wl in a0 ] )
-    else:
-        s = 0
-        for wl in a0:
-            a0_f, a0_e = a0[ wl ]
-            low, high = a0_f + n_sigma * a0_e, a0_f - n_sigma * a0_e
-            a1_f, a1_e = a1[ wl ]
-
-            # determine if errors overlap
-            if abs( a0_f - a1_f ) < n_sigma * (a0_e + a1_e):
-                continue
-
-            s += pow( a1_f - a0_f, 2 ) / a0_f
-    if get_count:
-        s = (s, len( a0 ))
-    return s
+    wls = set( primary.keys() ).intersection( secondary.keys() )
+    if wl_low is not None:
+        wls = filter( lambda x: x >= wl_low, wls )
+    if wl_high is not None:
+        wls = filter( lambda x: x <= wl_high, wls )
+    return sum( [ __chi_value( primary[ wl ], secondary[ wl ], n_sigma ) for wl in wls ] )
 
 
-def generic_chi( list0: list, list1: list ) -> float:
+def __multi_chi_wrapper( inputV: Tuple[ Spectrum, Spectrum, float, float, float ] ) -> Tuple[ str, float ]:
+    primary, secondary, wl_low, wl_high, n_sigma = inputV
+    return (secondary.getNS(), chi( primary, secondary, wl_low, wl_high, n_sigma ))
+
+
+def multi_chi_analysis( primary: Spectrum, speclist: Iterable[ Spectrum ], wl_low: float = None, wl_high: float = None,
+                        n_sigma: float = 1 ) -> Dict[ str, float ]:
     """
-    Returns the Chi^2 value between two lists of equal lengths
-
-    :param list0: Expected
-    :param list1: Observed
-    :return: chi^2
-    :rtype: float
-    :raises: AssertionError
-    """
-    if len( list0 ) != len( list1 ):
-        raise AssertionError( f"generic_chi: Lists are not of equal dimension\nLength list0: { len( list0 ) }\nLength list1: { len( list1 ) }")
-    _chi = 0
-    for i in range( len( list0 ) ):
-        _chi += pow( list0[ i ] - list1[ i ], 2 ) / list0[ 0 ]
-    return _chi
-
-@DeprecationWarning
-def async_chi_analysis( expSpec, speclist ):
-    async def __async_chi_wrapper( expSpec, obsSpec ):
-        return { obsSpec.getNS() : chi( expSpec, obsSpec ) }
-
-    from common.async_tools import generic_async_wrapper
-    import asyncio
-
-    results = []
-    chi_loop = asyncio.new_event_loop()
-    try:
-        chi_loop.run_until_complete( generic_async_wrapper( [ ( expSpec, spec ) for spec in speclist ], __async_chi_wrapper, results ) )
-    finally:
-        chi_loop.close()
-
-    resultsDict = {}
-    for result in results:
-        resultsDict.update( result )
-
-    return resultsDict
-
-
-def multi_chi_analysis( expSpec: Spectrum, speclist: List[ Spectrum ], getlist: bool = False, **kwargs ) -> Union[
-    List[ Tuple[ str, float ] ], dict ]:
-    """
-    Multiprocessing module for chi^2 analysis.  Returns a dictionary keyed by namestrings in the speclist with values of chi^2 result
-
-    :param expSpec: Expected Spectrum
-    :param speclist: list of Spectrum to perform analysis with expSpec
-    :return: Dictionary of { namestring : chi^2 value }, namestrings taken from the respective speclist entrys
+    A multiprocessing invoking method for a mass chi^2 analysis.  Given a primary spectrum and a wavelength range,
+    run a Chi^2 check against every spectrum in the passed speclist.  Chi^2 process is handed by the analysis.chi method
+    in this package.
+    
+    :param primary: Spectrum to be matched against
+    :type primary: Spectrum
+    :param speclist: Iterable of type Spectrum to be matched to primary
+    :type speclist: Iterable
+    :param wl_low:  Minimum wavelength to be used.  Defaults to None and thus will use individual object bounds if not passed.
+    :type wl_low: float
+    :param wl_high: Maximium wavelength to be used.  Same case as wl_low in default to None
+    :type wl_high: float
+    :param n_sigma: Error bound multiplier for defining the '0' range of the chi^2 process.  Defaults to 1.
+    :type n_sigma: float
+    :return: Namestring dictionary of { spectrum.getNS() : chi^2 value }
     :rtype: dict
     """
     from common.async_tools import generic_unordered_multiprocesser
 
-    results = []
-    input_values = [ (expSpec, spec, kwargs) for spec in speclist ]
+    results = [ ]
+    input_values = [ (primary, spec, wl_low, wl_high, n_sigma) for spec in speclist ]
     generic_unordered_multiprocesser( input_values, __multi_chi_wrapper, results )
-    if getlist:
-        return results
     return dict( results )
-
-def __multi_chi_wrapper( inputV ):
-    """
-    Wrapper for multi_chi_analysis, passed into multiprocessing module
-
-    :param inputV: tuple item from input_values list; unpacked in module to ( expSpec, obsSpec ) to pass into chi()
-    :type inputV: tuple
-    :return: Dictionary of { obsSpec.namestring : chi^2 result }
-    :rtype: dict
-    """
-    expSpec, obsSpec = inputV[ :2 ]
-    kwargs = { }
-    [ kwargs.update( pt ) for pt in inputV[ 2: ] ]
-    return (obsSpec.getNS( ), chi( expSpec, obsSpec, **kwargs ))
-
-def pipeline_chi_wrapper( inputV ):
-    return __multi_chi_wrapper( inputV )
-
-def em_chi_wrapper( inputV: Tuple[ Spectrum, Spectrum ] ) -> Dict[ str, float ]:
-    """
-    Wrapper for running chi^2 over EM lines alone.  Scales obsSpec to expSepc before trimming to EM lines
-
-    :param inputV: Tuple of ( expected_spectrum, observed_spectrum )
-    :type inputV: Tuple( Spectrum, Spectrum )
-    :return: Dictionary of { observed_spectrum.getNS() : chi( expected_spectrum, observed_spectrum )
-    :rtype: Dict[ str : float ]
-    """
-    from spectrum import drop_to_em_lines
-    expSpec, obsSpec = inputV
-    try:
-        obsSpec.scale( spec=expSpec )
-    except ZeroDivisionError:
-        print( "em_chi_wrapper: Unable to scale spectra - Is the default aveFlux range still in the spectrum?" )
-        print( expSpec, '\n', obsSpec )
-    expSpec, obsSpec = drop_to_em_lines( expSpec.cpy( ), obsSpec.cpy( ) )
-    return { obsSpec.getNS( ): chi( expSpec, obsSpec, doScale=False, skipCopy=True ) }
-
-
-def fwhm( spec: Spectrum, wl_range: Tuple[ float, float ] ) -> float:
-    spec = spec.cpy( )
-    spec.trim( wl_range=wl_range )
-
-    wls = spec.getWavelengths( )
-
-    max_wl = min_wl = wls[ 0 ]
-    min_flux = spec.getFlux( min_wl )
-    max_flux = spec.getFlux( max_wl )
-    for wl in wls:
-        if min_flux > spec.getFlux( wl ):
-            min_flux = spec.getFlux( wl )
-            min_wl = wl
-        elif max_flux < spec.getFlux( wl ):
-            max_flux = spec.getFlux( wl )
-            max_wl = wl
-
-    half_max_position = spec.nearest( (min_wl - max_wl) / 2 )
-
-    return 2 * (max_wl - half_max_position)
