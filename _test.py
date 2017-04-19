0ba6e742e938dbeb5f426be5a1b977a859c0fe59
christopher <christopher.r.wahl@gmail.com>
2017-04-19 12:00:46 -0400

    ARCHIVE: Added some plot documentation, added simple xy_scatter_plot method

--- _test.py
+++ _test.py
@@ -1,13 +1,25 @@
-from analysis.divide import divide
 from catalog import shenCat
-from fileio.spec_load_write import rspecLoader
+from fileio.list_dict_utils import namestring_dict_reader, namestring_dict_writer
+from tools.plot import xy_scatterplot
 
-s0 = rspecLoader( shenCat.keys()[ 0 ] )
-s1 = rspecLoader( shenCat.keys()[ 1 ] )
+inpath, infile = "/home/christopher/Desktop/103/", "53327-1928-103.csv"
+namestring_dict_writer( shenCat, inpath, "shenCat.csv" )
+indict = namestring_dict_reader( inpath, infile )
+for ns in indict:
+    indict[ ns ] = shenCat[ ns ]
+namestring_dict_writer( indict, inpath, infile )
+exit( 0 )
 
-d = divide( s0, s1 )
+plotx = [ ]
+ploty = [ ]
+plotyerr = [ ]
+all_x = [ shenCat[ ns ][ 'z' ] for ns in shenCat ]
+all_y = [ shenCat[ ns ][ 'ew_hb' ] for ns in shenCat ]
 
-from analysis.slope_fit import spectrum_linear_fit
+for ns in indict:
+    plotx.append( shenCat[ ns ][ 'z' ] )
+    ploty.append( shenCat[ ns ][ 'ew_hb' ] )
+    plotyerr.append( shenCat[ ns ][ 'ew_hb_err' ] )
 
-m, b = spectrum_linear_fit( d )
-print( f"y = {m} x + {b}" )
+xy_scatterplot( "", "", plotx, ploty, plotyerr, debug=True )
+xy_scatterplot( "", "", all_x, all_y, debug=True )  # , plotcolor="dark-red" )
--- tools/plot.py
+++ tools/plot.py
@@ -3,62 +3,94 @@ from typing import Callable, List, Union
 import Gnuplot
 
 from analysis.pipeline import results_pipeline
-from spectrum import Spectrum
+from spectrum import Iterable, Spectrum
 
 
-def __fix_outpath( path : str, filename : str = None ) -> str:
+def __fix_outpath( path: str, filename: str = None ) -> str:
     from fileio.utils import join
     path = join( path, filename ) if filename is not None else path
     return '"' + ''.join( [ r"\\" if char == "\\" else char for char in path ] ) + '"'
 
-def make_points_plotitem( x_data : list, y_data : list, error_data : list = None, title : str = "", point_type : int = 7, point_size : int = 1, color : str = None ) -> Gnuplot.Data:
+
+def make_points_plotitem( x_data: Iterable, y_data: Iterable, error_data: Iterable = None, title: str = "",
+                          point_type: int = 7, point_size: int = 1, color: str = None ) -> Gnuplot.Data:
     with_ = ""
     if error_data is not None:
         with_ = "yerrorbars"
 
-    with_ = f"{with_} pt {point_type} ps {point_size}"
+    with_ = f"{with_} points pt {point_type} ps {point_size}"
 
-    if color is not None:
-        with_ = f"{with_} lc\"{color}\""
-
-    if error_data is not None:
-        from numpy import array
-        return Gnuplot.Data( array( [ *zip( x_data, y_data, error_data ) ] ), title=title, with_=with_ )
-
-    return Gnuplot.Data( x_data, y_data, title = title, with_ = with_ )
-
-def make_line_plotitem( x_data : list, y_data : list, title : str = "", with_ : str = "lines", color : str = None ) -> Gnuplot.Data:
     if color is not None:
         with_ = f"{with_} lc \"{color}\""
 
-    return Gnuplot.Data( x_data, y_data, title = title, with_ = with_ )
+    if error_data is not None:
+        from numpy import array
+        return Gnuplot.Data( array( [ *zip( x_data, y_data, error_data ) ] ), with_=with_, title=title )
 
-def make_spectrum_plotitem( spec : Spectrum, color : str = None ) -> Gnuplot.Data:
+    return Gnuplot.Data( x_data, y_data, title=title, with_=with_ )
+
+
+def make_line_plotitem( x_data: Iterable, y_data: Iterable, title: str = "", with_: str = "",
+                        color: str = None ) -> Gnuplot.Data:
+    """
+    Takes two iterables of x and y data (order paired) and returns a line plot object in Gnuplot.Data format. 
+    
+    :param x_data: 
+    :type x_data: Iterable
+    :param y_data: 
+    :type y_data: Iterable
+    :param title:  Plot object title - what will be displayed in the key
+    :type title: str
+    :param with_: Customizable "with ..." string.  Will be concatenated with "... lc 'color'" if a color is passed.
+    :type with_: str
+    :param color: Gnuplot compatible color for the plot item.
+    :type color: str
+    :return: Gnuplot plotable data
+    :rtype: Gnuplot.Data
+    """
+    with_ = "lines " + with_
+    if color is not None:
+        with_ = f"{with_} lc \"{color}\""
+
+    return Gnuplot.Data( x_data, y_data, title=title, with_=with_ )
+
+
+def make_spectrum_plotitem( spec: Spectrum, color: str = None ) -> Gnuplot.Data:
+    """
+    Takes in a Spectrum class and returns Gnuplot.Data format for plotting using the data within the Spectrum object
+     
+    :param spec:
+    :type spec: Spectrum
+    :param color: 
+    :type color: str
+    :return: 
+    :rtype: Gnuplot.Data
+    """
     _with = "lines"
     if color is not None:
         _with = f'{_with} lc "{color}"'
-    return make_line_plotitem( spec.getWavelengths(), spec.getFluxlist(), title = spec.getNS(), with_= _with )
+    return make_line_plotitem( spec.getWavelengths(), spec.getFluxlist(), title=spec.getNS(), with_=_with )
 
 
 def four_by_four_multiplot( prime: Spectrum, speclist: list, path: str = None, filename: str = None,
                             plotTitle: str = "", debug: bool = False ) -> Union[ Gnuplot.Gnuplot, None ]:
     from common.constants import ANGSTROM, FLUX_UNITS
 
-    primeData = make_spectrum_plotitem( prime, color = "royalblue" )
-    coSpecs = [ make_spectrum_plotitem( spec, color = "black" ) for spec in speclist ]
+    primeData = make_spectrum_plotitem( prime, color="royalblue" )
+    coSpecs = [ make_spectrum_plotitem( spec, color="black" ) for spec in speclist ]
 
     g = Gnuplot.Gnuplot()
     g( 'set grid' )
     g( 'set key opaque box' )
     g.xlabel( u'Wavelength %s' % ANGSTROM )
-    g.ylabel( u'Flux Density %s' %FLUX_UNITS )
+    g.ylabel( u'Flux Density %s' % FLUX_UNITS )
     if not debug:
         g( "set terminal pdf enhanced color size 11, 8.5" )
-        g( f"set output {__fix_outpath( path, filename )}")
+        g( f"set output {__fix_outpath( path, filename )}" )
 
     for i in range( 0, len( coSpecs ), 4 ):
         g( f'set multiplot layout 2,2 title "{plotTitle}"' )
-        for coSpec in coSpecs[ i : i + 4 ]:
+        for coSpec in coSpecs[ i: i + 4 ]:
             g.plot( coSpec, primeData )
         g( 'unset multiplot' )
 
@@ -78,11 +110,11 @@ def ab_z_plot( path: str, filename: str, primary: Union[ str or Spectrum ],
 
     if primary is not None:
         if type( primary ) is Spectrum:
-            if primary.getNS( ) in shenCat:
-                p_z, p_ab, p_ab_err = shenCat.subkey( primary.getNS( ), 'z', 'ab', 'ab_err' )
+            if primary.getNS() in shenCat:
+                p_z, p_ab, p_ab_err = shenCat.subkey( primary.getNS(), 'z', 'ab', 'ab_err' )
             else:
-                p_z, p_ab, p_ab_err = primary.getRS( ), primary.magAB( ), primary.abErr( )
-            primary = primary.getNS( )
+                p_z, p_ab, p_ab_err = primary.getRS(), primary.magAB(), primary.abErr()
+            primary = primary.getNS()
         else:
             p_z, p_ab, p_ab_err = shenCat.subkey( primary, 'z', 'ab', 'ab_err' )
 
@@ -109,16 +141,16 @@ def ab_z_plot( path: str, filename: str, primary: Union[ str or Spectrum ],
                 ab_err.append( n_sigma * shenCat.subkey( ns, 'ab_err' ) )
         else:
             for spec in points:
-                z_data.append( shenCat.subkey( spec.getNS( ), 'z' ) )
-                ab_data.append( shenCat.subkey( spec.getNS( ), 'ab' ) )
-                ab_err.append( n_sigma * shenCat.subkey( spec.getNS( ), 'ab_err' ) )
+                z_data.append( shenCat.subkey( spec.getNS(), 'z' ) )
+                ab_data.append( shenCat.subkey( spec.getNS(), 'ab' ) )
+                ab_err.append( n_sigma * shenCat.subkey( spec.getNS(), 'ab_err' ) )
     elif type( points ) is dict:
         for ns in points:
             z_data.append( shenCat.subkey( ns, 'z' ) )
             ab_data.append( shenCat.subkey( ns, 'ab' ) )
             ab_err.append( n_sigma * shenCat.subkey( ns, 'ab_err' ) )
     elif isinstance( points, results_pipeline ):
-        for ns in points.get_results( ):
+        for ns in points.get_results():
             z_data.append( shenCat.subkey( ns, 'z' ) )
             ab_data.append( shenCat.subkey( ns, 'ab' ) )
             ab_err.append( n_sigma * shenCat.subkey( ns, 'ab_err' ) )
@@ -156,9 +188,10 @@ def ab_z_plot( path: str, filename: str, primary: Union[ str or Spectrum ],
 
     if not debug:
         g( 'set output' )
-        g.close( )
+        g.close()
         return None
     return g
+
 
 def spectrum_plot( spec: Spectrum, path: str, filename: str, color: str = "royalblue", debug: bool = False ) -> None:
     """
@@ -195,7 +228,60 @@ def spectrum_plot( spec: Spectrum, path: str, filename: str, color: str = "royal
             filename += ".pdf"
         g( f'set output {__fix_outpath( path, "%s" % filename )}' )
 
-    g.plot( make_spectrum_plotitem( spec, color = color ) )
+    g.plot( make_spectrum_plotitem( spec, color=color ) )
 
+    if not debug:
+        g( 'set output' )
+
+
+def xy_scatterplot( path: str, filename: str, x_data: Iterable, y_data: Iterable, y_error_bars: Iterable = None,
+                    dataTitle: str = None, plotTitle: str = None, x_label: str = None, y_label: str = None,
+                    plotcolor: str = "royalblue", debug: bool = False ) -> None:
+    """
+    Simple XY scatterplot production.  Plots using PDF terminal.  If passing debug=True, no file will be written to 
+    disk.  To avoid typing errors, it is desirable to pass "" for path and filename variables.
+    
+    :param path: /path/to/output file
+    :type path: str
+    :param filename: output file name
+    :type filename: str
+    :param x_data: 
+    :type x_data: Iterable
+    :param y_data: 
+    :type y_data: Iterable
+    :param y_error_bars:
+    :type y_error_bars: Iterable
+    :param dataTitle: Title of the xy data that will be displayed in the key.  If not passed, key will be removed from the plot.
+    :type dataTitle: str
+    :param plotTitle: 
+    :type plotTitle: str
+    :param x_label: X-axis label
+    :type x_label: str
+    :param y_label: Y-axis label
+    :type y_label: str
+    :param plotcolor: Color of plot points.  Must be a Gnupot compatible colorname such as "dark-red" 
+    :type plotcolor: str
+    :param debug: 
+    :type debug: bool
+    :return: 
+    :rtype: None
+    """
+    from fileio.utils import dirCheck
+
+    if not debug: dirCheck( path )
+    g = Gnuplot.Gnuplot( persist=debug )
+
+    if plotTitle is not None: g.title( plotTitle )
+    if x_label is not None: g.xlabel( x_label )
+    if y_data is not None: g.ylabel( y_label )
+    g( 'set grid' )
+    if dataTitle is None:
+        g( "unset key" )
+    if not debug:
+        g( 'set terminal pdf color enhanced size 9,6' )
+        if ".pdf" not in filename:
+            filename += ".pdf"
+        g( f'set output {__fix_outpath( path, "%s" % filename )}' )
+    g.plot( make_points_plotitem( x_data, y_data, y_error_bars, title=dataTitle, color=plotcolor ) )
     if not debug:
         g( 'set output' )
