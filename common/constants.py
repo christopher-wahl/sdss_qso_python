115d6493291de7a0dd45124ce765492ff28dc92a
christopher <christopher-wahl@users.noreply.github.com>
2017-05-01 14:54:40 -0400

    ARCHIVE: Restructured common.constants package to work with SDSS_QSO_DATA repository

--- common/constants.py
+++ common/constants.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from multiprocessing import cpu_count, freeze_support
 
 from common.messaging import BETA, GAMMA
@@ -13,19 +14,31 @@ MAX_PROC = cpu_count()
 
 """ BASE PATHS """
 def get_base_code_path( ) -> str:
+    """
+    This method finds the path of your code's root folder.  For example, the entirety of this project's code is housed
+    in a folder titled "pythonqso".  Regardless of where I start a program, I will be able to locate the code path within
+    that program by invoking either this method, or the constant BASE_CODE_PATH this is called by shortly afterwards.
+    :return: 
+    """
     path = os.getcwd( )
     while (os.path.split( path )[ 1 ] != "pythonqso"):
         path = os.path.split( path )[ 0 ]
     return os.path.abspath( path )
 
+
+ROOT_DRIVE_PATH = "/media/christopher/Research" if sys.platform == "linux" else "D:/"
+
 BASE_CODE_PATH = get_base_code_path( )
-BASE_PROCESSED_PATH = abspath( "../../Processed" )
-BASE_ANALYSIS_PATH = join( BASE_PROCESSED_PATH, "Analysis" )
-BASE_SPEC_PATH = join( BASE_PROCESSED_PATH, "Spec" )
-BINNED_SPEC_PATH = join( BASE_SPEC_PATH, "BIN SOURCE" )
-REST_SPEC_PATH = join( BINNED_SPEC_PATH, "REST" )
+if BASE_CODE_PATH not in sys.path:
+    sys.path.insert( 0, BASE_CODE_PATH )
+
+BASE_DATA_PATH = abspath( join( ROOT_DRIVE_PATH, "Data" ) )
+BASE_ANALYSIS_PATH = join( BASE_DATA_PATH, "Analysis" )
+BASE_SPEC_PATH = join( BASE_DATA_PATH, "Spec" )
 SOURCE_SPEC_PATH = join( BASE_SPEC_PATH, "Source" )
-BASE_PLOT_PATH = join( BASE_PROCESSED_PATH, "Plot" )
+BINNED_SPEC_PATH = join( BASE_SPEC_PATH, "BINNED" )
+REST_SPEC_PATH = join( BINNED_SPEC_PATH, "REST" )
+BASE_PLOT_PATH = join( BASE_DATA_PATH, "Plot" )
 
 """ DEFAULT VALUES """
 DEFAULT_SCALE_WL = 4767
@@ -45,5 +58,5 @@ RANGE_STRING_DICT = { MGII_RANGE: "MgII", HG_RANGE: f"H{GAMMA}", HB_RANGE: f"H{B
                       CONT_RANGE: "Continuum" }
 
 """ MISC VALUES """
-BASE_SOURCE_FILES_PATH = join( os.path.split( BASE_PROCESSED_PATH )[ 0 ], "Source" )
-SHEN_FIT_FILE = join( BASE_SOURCE_FILES_PATH, "shen", "dr7_bh_May_2011.fits" )
+SHEN_FOLDER = join( BASE_DATA_PATH, "Shen" )
+SHEN_FIT_FILE = join( SHEN_FOLDER, "dr7_bh_May_2011.fits" )
